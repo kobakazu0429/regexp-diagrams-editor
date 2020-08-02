d05b249eb62e671b291c30ecd4e071dab6695dc6
kobakakzu0429 <kobakazu10@gmail.com>
2020-08-02 16:16:56 +0900

    add sample react-diagrams

--- src/components/MainPane/index.tsx
+++ src/components/MainPane/index.tsx
@@ -1,5 +1,46 @@
 import React, { FC } from "react";
 import styled from "styled-components";
+import createEngine, {
+  DiagramModel,
+  DefaultNodeModel,
+  DefaultLinkModel
+} from "@projectstorm/react-diagrams";
+import { CanvasWidget } from "@projectstorm/react-canvas-core";
+
+const Sample: FC = () => {
+  //1) setup the diagram engine
+  const engine = createEngine();
+
+  //2) setup the diagram model
+  const model = new DiagramModel();
+
+  //3-A) create a default node
+  const node1 = new DefaultNodeModel({
+    name: "Node 1",
+    color: "rgb(0,192,255)"
+  });
+  node1.setPosition(100, 100);
+  const port1 = node1.addOutPort("Out");
+
+  //3-B) create another default node
+  const node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
+  const port2 = node2.addInPort("In");
+  node2.setPosition(400, 100);
+
+  // link the ports
+  const link1 = port1.link<DefaultLinkModel>(port2);
+  link1.getOptions().testName = "Test";
+  link1.addLabel("Hello World!");
+
+  //4) add the models to the root graph
+  model.addAll(node1, node2, link1);
+
+  //5) load model into engine
+  engine.setModel(model);
+
+  //6) render the diagram!
+  return <CanvasWidget engine={engine} />;
+};
 
 export const MainPane: FC = () => {
   return (
@@ -8,7 +49,7 @@ export const MainPane: FC = () => {
         <p>header/controller</p>
       </div>
       <EditorWrapper>
-        <p>editor/main</p>
+        <Sample />
       </EditorWrapper>
       <div>
         <p>result</p>
@@ -25,6 +66,11 @@ const Wrapper = styled.div`
 `;
 
 const EditorWrapper = styled.div`
+  > * {
+    height: 100%;
+    min-height: 100%;
+    width: 100%;
+  }
   height: 100%;
   background-color: rgb(60, 60, 60);
   background-size: 50px 50px;
